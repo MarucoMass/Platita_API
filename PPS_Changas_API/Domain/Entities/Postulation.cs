6c52ad81103ebbdd829ec9874053bff6a3827b39
patriciogazzera <patriciogazzera7@gmail.com>
2025-04-03 21:16:44 -0300

    Creacion de clases

---
+++ PPS_Changas_API/Domain/Entities/Postulation.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Postulation
+    {
+        public Job Job { get; set; }
+        public Employee Employee { get; set; }
+        public StatusEnum Status { get; set; }
+    }
+}
